237a6734e263ea37dbb6488841044c54f086d577
David Farlow <david.farlow@ellucian.com>
2021-11-28 22:58:21 -0500

    add logout and redirect

--- flaskpage/__init__.py
+++ flaskpage/__init__.py
@@ -8,4 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manger = LoginManager(app)
+login_manger.login_view = 'login'
+login_manger.login_message_category = 'info'
 from flaskpage import routes
